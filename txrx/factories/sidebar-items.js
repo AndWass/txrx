bf05d32e8ec847918039d1524251196eb1fe22eb
AndWass <AndWass@users.noreply.github.com>
2021-08-20 15:45:15 +0000

    deploy: c6b0e2ce87a98aaac3a7027ac544e4ae310cdba7

--- txrx/factories/sidebar-items.js
+++ txrx/factories/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["done",""],["just","Create a sender that, when started, immediately sends its value to the receiver."]],"mod":[["done",""],["futures",""],["just_sender",""],["on_scheduler",""]]});
+initSidebarItems({"fn":[["cancelled","A sender that only sends the `cancelled` signal."],["error","A sender that only sends the `error` signal."],["just","Create a sender that, when started, immediately sends its value to the receiver."]],"mod":[["futures",""],["immediate",""],["just_sender",""],["on_scheduler",""]]});
